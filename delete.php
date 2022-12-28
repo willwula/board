67e57c0df8aaecae7621388e0facf3442e74e5b8
willwula <wfh318@gmail.com>
2022-12-28 14:36:19 +0800

    feat:create delete.php

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+include "db.php";
+session_start();
+$no = $_GET['no'];
+$sql = "DELETE from guestbook where no='$no'";
+$name = $_SESSION['name'];
+$result = $pdo->prepare($sql);
+$result->execute();
+if (!$result) {
+    die();
+} else {
+    echo "
+         <script>
+            setTimeout(function(){window.location.href='view.php?name=" . $name . "&no=" . $no . "';},300);
+        </script>";
+
+}
+
+
